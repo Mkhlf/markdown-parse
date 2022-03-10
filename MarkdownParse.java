80756757946c678c6f66f7597c06f7dc1065d119
Mohammad Alkhalifah <56599163+Mkhlf@users.noreply.github.com>
2022-03-10 14:10:16 -0800

    replacing the old impl. with the new one.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -3,72 +3,47 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import org.commonmark.node.*;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+
+class LinkCounterVisitor extends AbstractVisitor {
+    int linkCount = 0;
+    ArrayList<String> links = new ArrayList<>();;
+
+    @Override
+    public void visit(Link link) {
+
+        // This is called for all Link nodes. Override other visit methods for other node types.
+
+        // Count links (this is just an example, don't actually do it this way for various
+        // reasons).
+        linkCount += 1;
+        String nextLink = link.getDestination();
+        links.add(nextLink);
+        // Descend into children (could be omitted in this case because Text nodes don't have
+        // children).
+        visitChildren(link);
+    }
+
+}
+
 
 public class MarkdownParse {
-    public static int indexOfUnescaped(String str, String search, int startIndex) {
-        int currIndex = startIndex - 1;
-        int backslashCount;
-        do {
-            currIndex = str.indexOf(search, currIndex + 1);
-            backslashCount = 0;
-            int index = currIndex - 1;
-            while (index >= 0 && str.charAt(index) == '\\') {
-                index--;
-                backslashCount++;
-            }
-        } while (backslashCount % 2 == 1);
-        return currIndex;
-    }
-
-    public static ArrayList<String> getLinksFromLine(String markdown) {
-        ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
-        // newline
-        int currentIndex = 0;
-        while (currentIndex < markdown.length()) {
-            int nextOpenBracket = indexOfUnescaped(markdown, "[", currentIndex);
-            if (nextOpenBracket == -1) {
-                break;
-            }
-            int nextCloseBracket = indexOfUnescaped(markdown, "]", nextOpenBracket);
-            int openParen = indexOfUnescaped(markdown, "(", nextCloseBracket);
-            if (openParen == -1) {
-                break;
-            }
-            int closeParen = indexOfUnescaped(markdown, ")", openParen);
-            int nextOpenParen = indexOfUnescaped(markdown, "(", openParen + 1);
-            if (nextOpenParen != -1 && closeParen > nextOpenParen) {
-                // Invalid link because there's an open paren before the close paren
-                int openBracket = indexOfUnescaped(markdown, "[", openParen);
-                if (openBracket != -1) {
-                    // Maybe there's another link
-                    currentIndex = openBracket;
-                    continue;
-                } else {
-                    break;
-                }
-            }
-            if (closeParen == -1) {
-                break;
-            }
-            // Check that this isn't an image link
-            if (!(nextOpenBracket > 0 && markdown.charAt(nextOpenBracket - 1) == '!')) {
-                String substr = markdown.substring(openParen + 1, closeParen);
-                substr = substr.replaceAll("\\\\(.)", "$1");
-                toReturn.add(substr);
-            }
-            currentIndex = closeParen + 1;
-        }
-        return toReturn;
-    }
 
     public static ArrayList<String> getLinks(String markdown) {
-        ArrayList<String> toReturn = new ArrayList<>();
-        for (String line : markdown.split("\n")) {
-            toReturn.addAll(getLinksFromLine(line));
-        }
-        return toReturn;
+        Parser parser = Parser.builder().build();
+
+        Node document = parser.parse("This is *Sparta*");
+
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+
+        Node node = parser.parse(markdown);
+        LinkCounterVisitor linkVisitor = new LinkCounterVisitor();
+        node.accept(linkVisitor);
+        return linkVisitor.links;
+
     }
 
     public static void main(String[] args) throws IOException {
